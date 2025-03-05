deff68df4e6c3f5c7e3b074ed3b28d3284e6ec1a
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2025-03-05 13:59:03 +0300

    assignment

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+
+# Create a new application function
+# First import the module Flask
+from flask import Flask
+#Import the db object
+from app.extensions import db,migrate,jwt
+
+
+
+#import migrate
+
+
+
+
+
+def create_app():
+
+    app = Flask(__name__) # Creat the app name and path in the parameter name
+      # Access the app
+    app.config.from_object('config.Config')
+    db.init_app(app) # Instantiate it
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+  
+   
+
+#Register models from the application file.
+    from app.models.author_model import Author
+
+
+    @app.route('/')  # Add a route and a decorator and the route must be on top of the decorator.
+    def index():
+        return 'Hello, My name is Musiimenta Agnes, I am doing python'
+    
+
+
+    return app  # Return the application instance
+# Then test whether the application is running.
+
+
+
+                     
+
+
