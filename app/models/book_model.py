a3fa95711759c058cc4b87879d68b19b7cb259ac
Akello mary <akellomary665@gmail.com>
2025-03-05 16:07:57 +0300

    authentication

---
+++ app/models/book_model.py
@@ -0,0 +1,35 @@
+
+     # Attributes should be related to the class mentioned
+from app.extensions import db
+from datetime import datetime
+
+
+class Book(db.Model):
+    __tablename__ = "Books"
+    id= db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String(30),nullable=False)
+    price= db.Column(db.String(20),nullable=False)
+    description= db.Column(db.String(50),nullable=False) 
+    image = db.Column(db.String(50),nullable=True)
+    publication_date = db.Column(db.Date,nullable = False)
+    isbn = db.Column(db.String(30),nullable=True,Unique=True)
+    pages = db.Column(db.String(30),nullable=False)
+    genre = db.Column(db.String(50),nullable=False)
+    unit_price = db.Column(db.String(20),nullable=False)
+    author_id =db.Column(db.Integer,db.ForeignKey("authors.id"))
+    company_id =db.Column(db.Integer,db.ForeignKey("companies.id"))
+    author = db.relationship('Author',backref = 'books')
+    company = db.relationship('Company',backref = 'books')
+    created_at = db.Column(db.DateTime, default= datetime.now())
+    updated_at = db.Column(db.DateTime,onupdate= datetime.now()) 
+    
+
+    def __init__(self,title,price,description,image,pages,unit_price):
+
+          self.title= title
+          self.price = price
+          self.description = description
+          self.image = image
+          self.pages= pages
+          self.unit_price = unit_price
+
---
+++ config.py
@@ -0,0 +1,2 @@
+class Config: #The connection string shd be in the config file
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/author_db"
